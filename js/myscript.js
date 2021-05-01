c99a3962ab0917df228c8d6aa3048afaa15ee03f
Miguel Ricaño <mricanho@gmail.com>
2021-05-01 12:44:21 -0500

    Fix responsiveness of home page

--- js/myscript.js
+++ js/myscript.js
@@ -84,7 +84,7 @@ const fx = new TextScramble(el);
 let counter = 0;
 const next = () => {
   fx.setText(phrases[counter]).then(() => {
-    setTimeout(next, 1800);
+    setTimeout(next, 4000);
   });
   counter = (counter + 1) % phrases.length;
 };
